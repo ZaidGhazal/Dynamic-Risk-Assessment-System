35291ee03b7583dce87d6e02f205a2621bf59016
Zaid Ghazal <zghazal@beyond.ai>
2022-10-28 21:33:17 +0300

    Add trian_model function to training.py

--- training.py
+++ training.py
@@ -1,14 +1,11 @@
 from flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
-import pickle
+from pickle import dump
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
-###################Load config.json and get path variables
+# Load config.json and get path variables
 with open('config.json', 'r') as f:
     config = json.load(f)
 
@@ -16,16 +13,39 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 model_path = os.path.join(config['output_model_path'])
 
 
-#################Function for training the model
+def import_data() -> pd.DataFrame:
+    """ Import data from path in config file
+
+    Returns
+    -------
+     Data imported
+    """
+    data = pd.read_csv(os.path.join(dataset_csv_path, "finaldata.csv"))
+    return data
+
+
+# Function for training the model
 def train_model():
+    """Train and save ML model as trainedmodel.pkl """
+    # Import data
+    data = import_data()
+    X = data.drop(columns=["corporation", "exited"])
+    y = data["exited"]
+
     # use logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                       intercept_scaling=1, l1_ratio=None, max_iter=100,
-                       multi_class='warn', n_jobs=None, penalty='l2',
-                       random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                       warm_start=False)
+    lr = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                            intercept_scaling=1, l1_ratio=None, max_iter=100,
+                            multi_class='ovr', n_jobs=None, penalty='l2',
+                            random_state=0, solver='liblinear', tol=0.0001, verbose=0,
+                            warm_start=False)
 
-    # fit the logistic regression to your data
+    # fit the logistic regression to data
+    lr.fit(X, y)
+
+    # write the trained model to workspace in a file called trainedmodel.pkl
+    path = os.path.join(model_path, "trainedmodel.pkl")
+    dump(lr, open(path, 'wb'))
 
 
-    # write the trained model to your workspace in a file called trainedmodel.pkl
+if __name__ == '__main__':
+    train_model()
