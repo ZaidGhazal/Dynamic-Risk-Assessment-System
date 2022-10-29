cdf0bb8963fbdd1296981401477a5dc83db3ad5d
Zaid Ghazal <zghazal@beyond.ai>
2022-10-29 13:38:06 +0300

    Add functions to diagnostic.py module

--- diagnostics.py
+++ diagnostics.py
@@ -1,51 +1,143 @@
-
-# import pandas as pd
-# import numpy as np
-# import timeit
+from time import time
+import subprocess
 import os
 import json
+from pickle import load
+import numpy as np
+import pandas as pd
+from typing import List
+import sys
 
 # Load config.json and get environment variables
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path'])
+deployment_dir = os.path.join(config['prod_deployment_path'])
+dataset_csv_dir = os.path.join(config['output_folder_path'])
 test_data_path = os.path.join(config['test_data_path'])
 
-# Function to get model predictions
 
+# get model predictions
+def model_predictions(data: pd.DataFrame) -> np.ndarray:
+    """
+    read the deployed model and a test dataset, calculate predictions
 
-def model_predictions():
-    # read the deployed model and a test dataset, calculate predictions
-    pass
-    # return  # return value should be a list containing all predictions
+    """
+
+    X = data.drop(columns=["corporation", "exited"])
+    y = data["exited"]
+
+    model_path = os.path.join(deployment_dir, "trainedmodel.pkl")
+    model = load(open(model_path, "rb"))
+    preds = model.predict(X)
+
+    # return value should be a list containing all predictions
+    return preds
+
 
 # Function to get summary statistics
+def dataframe_summary() -> List:
+    """calculate summary statistics for each column in the ingested dataset"""
+
+    data = pd.read_csv(os.path.join(dataset_csv_dir, "finaldata.csv"))
+    data = data.select_dtypes(include=np.number)
+    stats_list = []
+    for col in data.columns:
+        mean = data[col].mean()
+        median = data[col].median()
+        std = data[col].std()
+        stats_list.append((mean, median, std))
+
+    # return value should be a list containing all summary statistics
+    return stats_list
 
 
-def dataframe_summary():
-    # calculate summary statistics here
-    pass
-    # return  # return value should be a list containing all summary statistics
+def missing_data_percent() -> List:
+    """Check missing data percent for each column in list"""
+    data = pd.read_csv(os.path.join(dataset_csv_dir, "finaldata.csv"))
+    data = data.select_dtypes(include=np.number)
+    percents_list = (data.isnull().mean() * 100).values.tolist()
+    return percents_list
+
 
 # Function to get timings
+def execution_time() -> List:
+    """
+    calculate timing of training.py and ingestion.py
+    """
+    timing_list = []
+
+    # Time for ingestion execution
+    start = time()
+    subprocess.call(["python", "-m", "ingestion"])
+    end = time()
+    timing_list.append(np.round(end - start, 2))
+
+    # Time for training execution
+    start = time()
+    subprocess.call(["python", "-m", "training"])
+    end = time()
+    timing_list.append(np.round(end - start, 2))
+
+    # return a list of 2 timing values in seconds: ingestion & training
+    return timing_list
 
 
-def execution_time():
-    # calculate timing of training.py and ingestion.py
-    pass
-    # return  # return a list of 2 timing values in seconds
+def get_latest_version_pkg(name: str) -> str:
+    """Get the latest version of the given package"""
+    latest_version = str(
+        subprocess.run(
+            [
+                sys.executable,
+                '-m',
+                'pip',
+                'install',
+                '{}==random'.format(name)],
+            capture_output=True,
+            text=True))
+    latest_version = latest_version[latest_version.find(
+        '(from versions:') + 15:]
+    latest_version = latest_version[:latest_version.find(')')]
+    latest_version = latest_version.replace(' ', '').split(',')[-1]
+
+    return latest_version
+
 
 # Function to check dependencies
-
-
 def outdated_packages_list():
-    # get a list of
-    pass
+    """Print summery of the currently used dependencies versions and latest version available"""
+    dep_df = pd.DataFrame(
+        columns=[
+            "pkg_name",
+            "pkg_current_version",
+            "pkg_latest_version"])
+    with open("requirements.txt", "r") as txt:
+        pkgs = txt.read().split("\n")
+
+    for pkg in pkgs:
+        if pkg.strip() == "":
+            continue
+        # Get pkg name
+        name = pkg.split("==")[0]
+
+        # Get pkg latest version
+        latest_version = get_latest_version_pkg(name)
+
+        # Get current version
+        if pkg.find("==") != -1:
+            curr_version = pkg.split("==")[1]
+        else:
+            curr_version = latest_version
+
+        dep_df.loc[len(dep_df.index)] = [name, curr_version, latest_version]
+
+    print(dep_df)
 
 
-if __name__ == '__main__':
-    model_predictions()
-    dataframe_summary()
-    execution_time()
-    outdated_packages_list()
+# if __name__ == '__main__':
+    # model_predictions()
+    # print(dataframe_summary())
+    # print("---------------")
+    # print(missing_data_percent())
+    # print(execution_time())
+    # outdated_packages_list()
--- ingestion.py
+++ ingestion.py
@@ -33,7 +33,6 @@ def merge_multiple_dataframe():
         data = pd.concat([data, temp_df])
 
     files_ls = [os.path.basename(file) for file in files_ls]
-    print(files_ls)
     with open(os.path.join(output_folder_path, "ingestedfiles.txt"), "a") as txt:
         for filename in files_ls:
             now = datetime.now()
